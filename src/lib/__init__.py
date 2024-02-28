b8412eebf2f6d77e0e8ec568d400d5449d0a1463
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2024-02-28 16:57:01 +0800

    Added the library files.

---
+++ src/lib/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Name:        __init__.py
+#
+# Purpose:     The regular package init module to init the customer "lib".
+#
+# Author:      Yuancheng Liu
+#
+# Created:     2014/01/15
+# Copyright:   
+# License:     
+#-----------------------------------------------------------------------------
+
+"""
+Regular Package Info
+
+Name: lib
+
+Description:
+- provide the module used for the infra monitor hub's frontend web host and the 
+backend data base handler .
+
+Modules inclided in the current package: 
+
+1. ConfigLoader: 
+provide API to load the not stand text format config file's data.
+
+2. Log.py: 
+provide the addtional log function to do the program execution log archiving feature.
+
+3. UdoCom: 
+provide UDP communication API in the distribution system.
+
+"""
