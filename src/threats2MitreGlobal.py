d2236a7ec6cec89b22c81aac40c0bf45a75d3277
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2024-02-28 17:45:26 +0800

    Added the threats2MitreGlobal file.

---
+++ src/threats2MitreGlobal.py
@@ -0,0 +1,114 @@
+#-----------------------------------------------------------------------------
+# Name:        threats2MitreGlobal.py
+#
+# Purpose:     This module is used as a local config file to set constants, 
+#              global parameters which will be used in the other modules.
+#              
+# Author:      Yuancheng Liu
+#
+# Created:     2024/02/26
+# Version:     v_0.1.2
+# Copyright:   Copyright (c) 2023 LiuYuancheng
+# License:     MIT License
+#-----------------------------------------------------------------------------
+"""
+For good coding practice, follow the following naming convention:
+    1) Global variables should be defined with initial character 'g'
+    2) Global instances should be defined with initial character 'i'
+    2) Global CONSTANTS should be defined with UPPER_CASE letters
+"""
+
+import os, sys
+
+print("Current working directory is : %s" % os.getcwd())
+DIR_PATH = dirpath = os.path.dirname(__file__) if os.path.dirname(__file__) else os.getcwd()
+print("Current source code location : %s" % dirpath)
+APP_NAME = ('OpenAI', 'mitre_mapper')
+
+TOPDIR = 'src'
+LIBDIR = 'lib'
+
+#-----------------------------------------------------------------------------
+# Init the logger:
+idx = dirpath.find(TOPDIR)
+gTopDir = dirpath[:idx + len(TOPDIR)] if idx != -1 else dirpath   # found it - truncate right after TOPDIR
+# Config the lib folder 
+gLibDir = os.path.join(gTopDir, LIBDIR)
+if os.path.exists(gLibDir):
+    sys.path.insert(0, gLibDir)
+import Log
+Log.initLogger(gTopDir, 'Logs', APP_NAME[0], APP_NAME[1], historyCnt=100, fPutLogsUnderDate=True)
+
+# Init the log type parameters.
+DEBUG_FLG   = False
+LOG_INFO    = 0
+LOG_WARN    = 1
+LOG_ERR     = 2
+LOG_EXCEPT  = 3
+
+def gDebugPrint(msg, prt=True, logType=None):
+    if prt: print(msg)
+    if logType == LOG_WARN:
+        Log.warning(msg)
+    elif logType == LOG_ERR:
+        Log.error(msg)
+    elif logType == LOG_EXCEPT:
+        Log.exception(msg)
+    elif logType == LOG_INFO or DEBUG_FLG:
+        Log.info(msg)
+
+#-----------------------------------------------------------------------------
+# load the config file.
+import ConfigLoader
+CONFIG_FILE_NAME = 'config.txt'
+gGonfigPath = os.path.join(dirpath, CONFIG_FILE_NAME)
+iConfigLoader = ConfigLoader.ConfigLoader(gGonfigPath, mode='r')
+if iConfigLoader is None:
+    print("Error: The config file %s is not exist.Program exit!" %str(gGonfigPath))
+    exit()
+CONFIG_DICT = iConfigLoader.getJson()
+
+#-----------------------------------------------------------------------------
+# Init the openAI parameters.
+API_KEY = CONFIG_DICT['API_KEY']
+os.environ["OPENAI_API_KEY"] = API_KEY
+AI_MODEL = CONFIG_DICT['AI_MODEL']
+
+
+# Attack scenario analyze prompt to parse the attack behaviors
+gSceAnalysePrompt="""
+Check the given cyber attack scenario description and split the attack flow path to a 
+list of attack behaviors.Please use the following format: 
+Attack behavior:
+1.attack behavior 1
+2.attack behavior 2
+...
+"""
+
+# Prompt used to map behaviours to the MITRE tactic and technique
+gBeh2MitrePrompt = """
+You are a helpful assistant who help mapping the cyber attack behavior description to the 
+tactic and technique in MITRE Adversarial Tactics, Techniques, and Common Knowledge (ATT&CK) Enterprise Matrix
+Please list the tactic and technique can match the attack behavior under below format:
+tactic: 
+technique: 
+"""
+
+# Prompt used to verify whether the technique can be applied/found direcly to the attack scenario.
+gMitreVerifyPrompt = """
+Verify whether the given MITRE ATT&CK technique can be found from the attack scenario: 
+%s
+
+Description and give a short explanation about the given technique can match
+which part of the scenario description. Please use the below answer format:
+Match: Yes/No
+Explanation: <Given technique can match which part of the scenario description>
+"""
+
+# Prompt used to test map scenario directly to the  
+gSce2MitrePrompt = """
+You are a helpful assistant who help mapping the cyber attack scenario description's 
+attack behavior to the tactic and technique in MITRE Adversarial Tactics, Techniques, 
+and Common Knowledge (ATT&CK) Enterprise Matrix. Please list tactic and technique in Enterprise Matrix.
+"""
+
